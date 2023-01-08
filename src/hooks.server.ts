ae1df991b7e21481606e87f0817ffd284eabaa4a
evariste1963 <evariste@hotmail.co.uk>
2023-01-08 17:21:50 +0000

    16:39 --> almost there

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,26 @@
-import type { Handle } from "@sveltejs/kit"
+import { authenticateUser } from "$lib/server/auth"
+import { redirect, type Handle } from "@sveltejs/kit"
+import { is_function } from "svelte/internal"
 
 export const handle: Handle = async ({ event, resolve }) => {
-	return resolve(event)
+	//stage 1
+	event.locals.user = authenticateUser(event)
+
+	if (event.url.pathname.startsWith('/protected')) {
+		if (!event.locals.user){
+			throw redirect(303, '/')
+		}
+		if (event.url.pathname.startsWith('/protected/admin')){
+			if(event.locals.user.role !== "ADMIN"){
+				throw redirect(303,'/protected')
+			}
+		}
+	}
+
+
+	// stage 2
+const response = await resolve(event)	
+
+	//stage 3
+	return response
 }
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,9 @@
 import { redirect } from "@sveltejs/kit"
 import type { Actions, PageServerLoad } from "./$types"
 
-export const load: PageServerLoad = async () => {}
+export const load: PageServerLoad = async () => {
+	
+}
 
 export const actions: Actions = {
 	login: async ({ cookies }) => {
