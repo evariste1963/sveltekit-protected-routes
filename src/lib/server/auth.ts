06cf7758329611253443c41cf3e285017bdde4e2
evariste1963 <evariste@hotmail.co.uk>
2023-01-08 20:19:39 +0000

    all done and very interesting it was too

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,31 +1,31 @@
-import type { RequestEvent } from "@sveltejs/kit"
+import type { RequestEvent } from '@sveltejs/kit';
 
 export const authenticateUser = (event: RequestEvent) => {
 	// get the cookies from the request
-	const { cookies } = event
+	const { cookies } = event;
 
 	// get the user token from the cookie
-	const userToken = cookies.get("auth")
+	const userToken = cookies.get('auth');
 
 	// if the user token is not valid, return null
 	// this is where you would check the user token against your database
 	// to see if it is valid and return the user object
-	if (userToken === "regularusertoken") {
+	if (userToken === 'regularusertoken') {
 		const user = {
 			id: 1,
-			email: "user@example.com",
-			role: "USER",
-		}
-		return user
+			email: 'user@example.com',
+			role: 'USER'
+		};
+		return user;
 	}
-	if (userToken === "adminusertoken") {
+	if (userToken === 'adminusertoken') {
 		const user = {
 			id: 2,
-			email: "admin@example.com",
-			role: "ADMIN",
-		}
-		return user
+			email: 'admin@example.com',
+			role: 'ADMIN'
+		};
+		return user;
 	}
 
-	return null
-}
+	return null;
+};
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,22 @@
-import { redirect } from "@sveltejs/kit"
-import type { Actions, PageServerLoad } from "./$types"
+import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-	
-}
+export const load: PageServerLoad = async ({ locals }) => {
+	return {
+		user: locals.user
+	};
+};
 
 export const actions: Actions = {
 	login: async ({ cookies }) => {
-		cookies.set("auth", "regularusertoken", {
-			path: "/",
+		cookies.set('auth', 'adminusertoken', {
+			path: '/',
 			httpOnly: true,
-			sameSite: "strict",
-			secure: process.env.NODE_ENV === "production",
-			maxAge: 60 * 60 * 24 * 7, // 1 week
-		})
+			sameSite: 'strict',
+			secure: process.env.NODE_ENV === 'production',
+			maxAge: 60 * 60 * 24 * 7 // 1 week
+		});
 
-		throw redirect(303, "/")
-	},
-}
+		throw redirect(303, '/');
+	}
+};
